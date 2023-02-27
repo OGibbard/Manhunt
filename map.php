20e8fc759c9e0c8e22e5e7238520d8c29a700d81
ogibbard <gibbard.o@oundleschool.org.uk>
2023-02-27 11:48:06 +0000

    done

--- map.php
+++ map.php
@@ -23,39 +23,68 @@ if (isset($_SESSION['name'])==false){
 
     <script>
     const successCallback = (position) => {
-        console.log(position);
     var username = '<?php echo $_SESSION['name'];?>';
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     var accuracy = position.coords.accuracy;
     var latest = position.timestamp;
+
+    var a = new Date(latest);
+    var year = a.getFullYear();
+    var month = a.getMonth();
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var fulldate = date + '/' + month + '/' + year
+    var time = hour + ':' + min + ':' + sec
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open('GET','uploadlocation.php?latitude='+latitude+'&longitude='+longitude+'&accuracy='+accuracy+'&latest='+latest+'&username='+username, true);
     xmlhttp.send();
-    console.log(position);
 
     var people = (<?php echo $people;?>);
     let map;
     function initMap() {
     // The map, centered at Thames and Kennet
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 6,
-      center: {lat: 54.3760767, lng: -2.5588948},
+      zoom: 13,
+      center: {lat: 52.481149, lng: -0.485304},
       streetViewControl: false,
     });
     
     for (let j=0; j < people.length; j++) {
+      const infowindow = new google.maps.InfoWindow({
+        content: ('<a>Date: '+fulldate+'</a><br><a>Time: '+time+'</a><br><a>Accuracy: '+accuracy+'</a>'),
+      });
       const marker = new google.maps.Marker({
         position: {lat: parseFloat(people[j].Latitude), lng: parseFloat(people[j].Longitude)},
         label: people[j].Username,
         map: map,
       });
+      marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
       };
+    const infowindow = new google.maps.InfoWindow({
+      content: ('<a>Date: '+fulldate+'</a><br><a>Time: '+time+'</a><br><a>Accuracy: '+accuracy+'</a>'),
+    });
     const marker = new google.maps.Marker({
         position: {lat: latitude, lng: longitude},
         label: 'Me',
         map: map,
     });
+    marker.addListener("click", () => {
+        infowindow.open({
+          anchor: marker,
+          map,
+          shouldFocus: false,
+        });
+      });
     }
 
     window.initMap = initMap;
@@ -82,15 +111,14 @@ if (isset($_SESSION['name'])==false){
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzpVcQynoGYjwPIJi4gAoIPvFBXI30J6w&callback=initMap&v=weekly"
     ></script>
     <br><br>
-    <h4>Last coordinates</h4>
     <?php
-		include_once("connection.php");
-		$stmt = $conn->prepare("SELECT * FROM players");
-		$stmt->execute();
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $timestamp = $row['Latest'];
-			echo ($row["Username"] . ': ' . $timestamp.'<br>');
-		}
+		#include_once("connection.php");
+		#$stmt = $conn->prepare("SELECT * FROM players");
+		#$stmt->execute();
+		#while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    #        $timestamp = date('Y-m-d h:i:s',($row['Latest']-3600000)/1000);
+		#	echo ($row["Username"] . ': ' . $timestamp.'    Accuracy: '.$row['Accuracy'].'<br>');
+		#}
 		?>
   </body>
 </html>
